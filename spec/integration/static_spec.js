32940497378a121b483b3388e9ca65102851001f
Richard Tinajero <rj.tinajero92@gmail.com>
2019-06-11 11:26:57 -0700

    initial setup done

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,18 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+  describe("GET /", () => {
+
+    it("should return status code 200 and say 'Book Table'", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Book Table");
+        done();
+      });
+    });
+
+  });
+});

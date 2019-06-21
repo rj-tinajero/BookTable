dafc0543b4f081d25778e2207edd9a630785072f
Richard Tinajero <rj.tinajero92@gmail.com>
2019-06-21 10:58:51 -0700

    acct update

---
+++ src/controllers/userController.js
@@ -0,0 +1,48 @@
+const userQueries = require("../db/queries.users.js");
+const passport = require("passport");
+
+module.exports = {
+   signUp(req, res, next) {
+      res.render("users/sign_up");
+   },
+   create(req, res, next) {
+      let newUser = {
+         first_name: req.body.first_name,
+         last_name: req.body.last_name,
+         email: req.body.email,
+         password: req.body.password,
+         passwordConfirmation: req.body.passwordConfirmation,
+         role: "customer"
+      };
+      userQueries.createUser(newUser, (err, user) => {
+         if(err) { console.log(err, "err in controller");
+            req.flash("error", err);
+            res.redirect("users/sign_up");
+         } else {
+            passport.authenticate("local")(req, res, () => {
+               req.flash("notice", "You've successfully signed in!");
+               res.redirect("/");
+             })
+         }
+      });
+   },
+   signInForm(req, res, next) {
+      res.render("users/sign_in")
+   },
+   signIn(req, res, next) {
+      passport.authenticate("local")(req, res, function() {
+         if(!req.user){
+           req.flash("notice", "Sign in failed. Please try again.")
+           res.redirect("/users/sign_in");
+         } else {
+           req.flash("notice", "You've successfully signed in!");
+           res.redirect("/");
+         }
+       })
+   },
+   signOut(req, res, next) {
+      req.logout();
+      req.flash("notice", "You signed out!");
+      res.redirect("/");
+   }
+}
---
+++ src/db/migrations/20190619204133-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate:{
+          isEmail: { msg: "must be a valid email" }
+        }
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        default: "customer"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
